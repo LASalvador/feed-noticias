61221983a7f08f97491c467857e8b78bdcc67611
Lucas Salvador <lucas.salvador11358@gmail.com>
2020-04-28 19:46:06 -0300

    removendo importações não utilizadas

--- src/main/java/feed/noticias/controller/UsuarioController.java
+++ src/main/java/feed/noticias/controller/UsuarioController.java
@@ -6,10 +6,6 @@ package feed.noticias.controller;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
-
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 
 import feed.noticias.model.UsuarioDAO;
